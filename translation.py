18527f13d0c7909de7862108f0bdc112ecd810b6
EthicalXGamer <155059899+EthicalXGamer@users.noreply.github.com>
2024-02-10 13:39:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,15 +1,14 @@
 class Translation(object):
 
       # Start text
-      START = "<b>Hi 👋 I Am A Channel Auto Forward Bot of <a href='https://t.me/VJ_Botz'>VJ Botz</a><b>. \n<b>Make your own bot now 👉 <a href='https://youtube.com/@Tech_VJ'>Tutorial Video</a><b> \n\n<b>Support Channel and Groups:</b>\n @VJ_Bots, @VJ_Bot_disscussion \n\n<b>About Bot:</b>\n Give Command /about"             
+      START = "<b>Hi 👋 I Am A Channel Auto Forward Bot of <a href='https://t.me/Ethical_Botz'>⏤͟͞  ＭＸ ＯＮＥ𓆪</a><b>. \n<b>Make your own bot now 👉 <a href='https://t.me/Ethical_Gamer/'>CREATOR 😈</a><b> \n\n<b>Support Channel and Groups:</b>\n @Ethical_Botz, @ethicalmusicsupport \n\n<b>About Bot:</b>\n Give Command /about"             
 
       #About text
       ABOUT = """<b>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟
-‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/VJ_Botz>VJ Auto Forward Bot 🇮🇳</a>
+‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Ethical_Botz>Ethical Botz</a>
  ‣ My best friend : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
- ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/kingvj01'>Tech VJ 🇮🇳</a> 
+ ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/anonymous_O_1/'> 😈⃤ ×°Ⱥղօղվʍօմʂ°× 😈⃤</a> 
  ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
  ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
  ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
- ‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://render.com'>Render</a> 
  ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]></b>"""
